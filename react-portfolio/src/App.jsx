c23e382d1438ba1bc2270b5d4007c47fc583af5a
Abdulganiyu-Abdulazeez <abdulganiyuabdulazeez20@gmail.com>
2024-12-23 00:39:25 +0100

    Skill first half

--- react-portfolio/src/App.jsx
+++ react-portfolio/src/App.jsx
@@ -1,11 +1,13 @@
 import LandingPage from './component/LandingPage'
 import About from './component/About';
+import Skill from './component/Skill';
 function App() {
 
   return (
     <>
       <LandingPage />
       <About />
+      <Skill />
     </>
   )
 }
