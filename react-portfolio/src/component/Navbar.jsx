18c2f32d6f22a87147ee6fc5ac4d7e12b4aa5d76
Abdulganiyu-Abdulazeez <abdulganiyuabdulazeez20@gmail.com>
2025-02-06 01:43:11 +0100

    navbar branch edit

--- react-portfolio/src/component/Navbar.jsx
+++ react-portfolio/src/component/Navbar.jsx
@@ -4,11 +4,11 @@ const Navbar = () => {
     return (
         <>
             <div className="flex w-full fit gap-[2rem] items-center justify-between bg-[#070d1b] p-[1rem] border-b-[1px] border-b-gray-700 relative">
-                <div class="flex items-center gap-[0.5rem]">
-                    <div class="rounded-[50%] border-[3px] border-solid border-[#fec544] w-[4rem] h-[4rem] p-[0.6rem] overflow-hidden flex items-center justify-center">
-                        <img class="w-full h-full" src="../src/assets/images/portfolio Logo.svg" alt="" />
+                <div className="flex items-center gap-[0.5rem]">
+                    <div className="rounded-[50%] border-[3px] border-solid border-[#fec544] w-[4rem] h-[4rem] p-[0.6rem] overflow-hidden flex items-center justify-center">
+                        <img className="w-full h-full" src="../src/assets/images/portfolio Logo.svg" alt="" />
                     </div>
-                    <h1 class="text-[40px] text-white"><b><span class="text-[#fec544]">A</span>bdulazeez</b></h1>
+                    <h1 className="text-[40px] text-white"><b><span className="text-[#fec544]">A</span>bdulazeez</b></h1>
                 </div>
                 <div className="flex md:static absolote items-center gap-[2rem]">
                     <p className="text-bold text-white cursor-pointer hover:text-[#ffc848]">Home</p>
