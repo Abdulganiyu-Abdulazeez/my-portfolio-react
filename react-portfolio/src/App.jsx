8584a6e30f5b1e61ed38112f8a8b6acc41780a07
Abdulganiyu Abdulazeez <abdulganiyuabdulazeez20@gmail.com>
2025-05-08 07:06:59 +0100

    conflicting dubble project component

--- react-portfolio/src/App.jsx
+++ react-portfolio/src/App.jsx
@@ -2,7 +2,6 @@ import LandingPage from './component/LandingPage'
 import About from './component/About';
 import Skill from './component/Skill';
 import Resume from './component/Resume';
-import Projects from './component/Projects';
 import Contact from './component/Contact';
 import Projects from './component/project';
 function App() {
